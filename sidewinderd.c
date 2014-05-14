eb4dda357e4155c2860842a24cdca4fc95e3192c
Tolga Cakir <tolga@cevel.net>
2014-05-14 23:03:11 +0200

    added profile switch function and global struct.

--- sidewinderd.c
+++ sidewinderd.c
@@ -20,14 +20,19 @@
 #define PRODUCT_ID_SIDEWINDER_X6	0x074b
 #define PRODUCT_ID_SIDEWINDER_X4	0x0768
 
-/* libusb variables */
-static libusb_device_handle *handle;
-static libusb_context *context = NULL;
+/* struct for Sidewinder data */
+struct sidewinder_data {
+	libusb_device_handle *handle;
+	libusb_context *context;
+	uint8_t profile;
+	uint8_t status;
+};
 
 int is_sidewinder(libusb_device *dev)
 {
 	struct libusb_device_descriptor desc;
-	int r = libusb_get_device_descriptor(dev, &desc);
+
+	libusb_get_device_descriptor(dev, &desc);
 
 	if (desc.idVendor == VENDOR_ID_MICROSOFT && desc.idProduct == PRODUCT_ID_SIDEWINDER_X6) {
 		return 1;
@@ -40,14 +45,14 @@ int is_sidewinder(libusb_device *dev)
 	return 0;
 }
 
-void sidewinder_init() {
+void sidewinder_init(struct sidewinder_data *sw) {
 	libusb_device **list;
 	libusb_device *found = NULL;
 
-	libusb_init(&context);
-	libusb_set_debug(context, 3);
+	libusb_init(&sw->context);
+	libusb_set_debug(sw->context, 3);
 
-	ssize_t cnt = libusb_get_device_list(context, &list);
+	ssize_t cnt = libusb_get_device_list(sw->context, &list);
 	ssize_t i = 0;
 	int err = 0;
 	if (cnt < 0) {
@@ -67,18 +72,18 @@ void sidewinder_init() {
 
 	if (found) {
 		printf("found usb-dev-block!\n");
-		err = libusb_open(found, &handle);
+		err = libusb_open(found, &sw->handle);
 		if (err) {
 			printf("Unable to open usb device\n");
 			error();
 		}
 
-		if (libusb_kernel_driver_active(handle, 1)) {
-			libusb_detach_kernel_driver(handle, 1);
+		if (libusb_kernel_driver_active(sw->handle, 1)) {
+			libusb_detach_kernel_driver(sw->handle, 1);
 			printf("Device busy...detaching...\n");
 		} else printf("Device free from kernel\n");
 
-		err = libusb_claim_interface(handle, 1);
+		err = libusb_claim_interface(sw->handle, 1);
 		if (err) {
 			printf("Failed to claim interface.");
 			switch (err) {
@@ -94,30 +99,50 @@ void sidewinder_init() {
 	}
 }
 
-void sidewinder_exit() {
-	if (handle) {
-		libusb_release_interface(handle, 1);
-		libusb_attach_kernel_driver(handle, 1);
-		libusb_close(handle);
+void sidewinder_control(struct sidewinder_data *sw, uint8_t *request) {
+	libusb_control_transfer(sw->handle, LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE | LIBUSB_ENDPOINT_OUT,
+			LIBUSB_REQUEST_SET_CONFIGURATION, 0x307, 0x1, request, 0x2, 0);
+}
+
+void sidewinder_switch_profile(struct sidewinder_data *sw) {
+	if (sw->profile < 0 || sw->profile > 1) {
+		sw->profile = 0;
+	} else {
+		sw->profile++;
 	}
 
-	if (context) {
-		libusb_exit(context);
+	uint8_t request[2];
+	request[0] = 0x7;
+	request[1] = 0x4 << sw->profile;
+
+	sidewinder_control(sw, request);
+}
+
+void sidewinder_exit(struct sidewinder_data *sw) {
+	if (sw->handle) {
+		libusb_release_interface(sw->handle, 1);
+		libusb_attach_kernel_driver(sw->handle, 1);
+		libusb_close(sw->handle);
 	}
 
-	handle = NULL;
-	context = NULL;
+	if (sw->context) {
+		libusb_exit(sw->context);
+	}
+
+	free(sw);
 }
 
 int main(int argc, char **argv) {
-	uint8_t data[2];
-	data[0] = 0x7;
-	data[1] = 0x4;
-	sidewinder_init();
+	struct sidewinder_data *sw;
+	sw = calloc(4, sizeof(struct sidewinder_data));
 
-	libusb_control_transfer(handle, LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE | LIBUSB_ENDPOINT_OUT,
-							LIBUSB_REQUEST_SET_CONFIGURATION, 0x307, 0x1, data, sizeof(data), 0);
+	sidewinder_init(sw);
+/*
+	while (1) {
+		libusb_handle_events_completed(sw->context, completed);
+	}
+*/
+	sidewinder_exit(sw);
 
-	sidewinder_exit();
 	exit(0);
 }
