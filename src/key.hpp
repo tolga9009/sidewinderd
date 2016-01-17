15ecb757abd80a1e45a6bf279a43ff96a0cb8f39
Tolga Cakir <tolga@cevel.net>
2016-01-17 22:28:05 +0100

    bumped year to 2016

--- src/key.hpp
+++ src/key.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014 - 2015 Tolga Cakir <tolga@cevel.net>
+ * Copyright (c) 2014 - 2016 Tolga Cakir <tolga@cevel.net>
  *
  * This source file is part of Sidewinder daemon and is distributed under the
  * MIT License. For more information, see LICENSE file.
